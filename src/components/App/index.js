18d5504111a41bf3d592abe452088bfb3d15a258
Irena Jurasek <irenaskydive@gmail.com>
2020-10-26 10:26:26 +0100

    Session handling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
@@ -9,24 +9,48 @@ import HomePage from '../Home';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
+import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
- 
-const App = () => (
-    <Router>
-      <div>
-        <Navigation />
-   
-        <hr />
-   
-        <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-        <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} />
-      </div>
-    </Router>
-  );
- 
-export default App;
+
+class App extends Component {
+  state = {
+    authUser: null
+  }
+
+  componentDidMount() {
+    console.log(this.props.firebase)
+    // Firebase offers a listener function to get the 
+    // authenticated user from Firebase
+    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null })
+    });
+  }
+
+  componentWillUnmount() {
+    this.listener();
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation authUser={this.state.authUser} />
+
+          <hr />
+
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default withFirebase(App);
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,5 @@
+const PasswordForget = () => {
+    return ( <div></div> );
+}
+ 
+export default PasswordForget;
